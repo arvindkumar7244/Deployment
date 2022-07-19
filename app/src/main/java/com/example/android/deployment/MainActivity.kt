21cf5639a3308c011fa204e8e51eba53f79cb1cb
Arvind Kumar <arvindkumar72446@gmail.com>
2022-07-19 17:00:01 +0530

    Added log in on create

--- app/src/main/java/com/example/android/deployment/MainActivity.kt
+++ app/src/main/java/com/example/android/deployment/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.android.deployment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TAG", "onCreate: Added on create log")
     }
 }
